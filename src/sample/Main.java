7372f3bacd9f62370e5ec99b197f183653020abb
nimeifu <gliu3637@bths.edu>
2019-01-02 12:29:31 -0500

    button done adding action

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 
-import java.awt.*;
 
 public class Main extends Application {
 
@@ -16,8 +18,17 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
 
-        Button button=new Button("Button");
-        Scene scene=new Scene(root,100,100);
+        Label label=new Label("");
+        Button button=new Button("click me!");
+        button.setStyle("-fx-text-fill: #0000ff");
+
+        button.setOnAction(value ->
+        {
+            return getClickCount(button);
+        });
+        HBox hbox=new HBox(button,label);
+        Scene scene=new Scene(hbox,500,500);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
@@ -25,4 +36,8 @@ public class Main extends Application {
     public static void main(String[] args) {
         Application.launch(args);
     }
+
+
+
+
 }
